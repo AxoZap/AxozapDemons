c655bc53c1d09e4332bb478d21117e2b43ecc83c
AxoZap <peteystillwell@icloud.com>
2026-05-13 11:38:01 -0400

    Disable summer 2026 page route

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
-import { Summer2026Page } from './components/Summer2026Page'
 import './styles/globals.css'
-
-const normalizedPath = window.location.pathname.replace(/\/+$/, '').toLowerCase()
-const isSummerCountdownPage = normalizedPath === '/summer2026'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    {isSummerCountdownPage ? <Summer2026Page /> : <App />}
+    <App />
   </StrictMode>,
 )
