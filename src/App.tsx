bd670bfd36317ecf4925fcc96fba1b599c70f20b
AxoZap <peteystillwell@icloud.com>
2026-06-29 13:17:46 -0400

    fix: import useMemo

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useMemo } from 'react';
 import { DemonList } from './components/DemonList';
 import { AddDemonForm } from './components/AddDemonForm';
 import { EditDemonForm } from './components/EditDemonForm';
